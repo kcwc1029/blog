9c7cddfe1ac986b951193bbb191f4f607e5de7d9
CHEN WEI-CHENG <3331363@gmail.com>
2024-11-18 20:36:13 +0800

    2024.10.18

--- 互動網頁設計/Vue/vue-project-03/src/components/stores/TaskStore.js
+++ 互動網頁設計/Vue/vue-project-03/src/components/stores/TaskStore.js
@@ -1,18 +1,50 @@
 import { defineStore } from "pinia";
-import { ref, computed } from "vue";
+import { ref, reactive, computed } from "vue";
 
 // 定義 Pinia Store
 export const useAlertsStore = defineStore("counter", () => {
-    const count = ref(0);
+    // TODO: state
+    const count = ref(1);
     const name = ref("TA");
+    const tasks = reactive([
+        {
+            id: 1,
+            title: "TA1",
+            isCompleted: false,
+        },
+        {
+            id: 2,
+            title: "TA2",
+            isCompleted: true,
+        },
+        {
+            id: 3,
+            title: "TA3",
+            isCompleted: true,
+        },
+    ]);
 
-    // 使用 computed 來建立計算屬性
+    // TODO: getters
+    const addTask = (title) => {
+        tasks.push({
+            id: tasks.length + 1,
+            title: title,
+            isCompleted: false,
+        });
+    };
+    const toggleTaskCompletion = (id) => {
+        const task = tasks.find((task) => task.id === id);
+        console.log("被toggle", task);
+    };
+
+    const removeTask = (id) => {
+        const index = tasks.findIndex((task) => task.id === id);
+        if (index !== -1) tasks.splice(index, 1); // 刪除1個元素
+    };
+    // TODO: getters
     const doubleCount = computed(() => count.value * 2);
-
-    // 定義方法
     const increment = () => {
         count.value++;
     };
-
-    return { count, name, doubleCount, increment };
+    return { name, tasks, count, doubleCount, increment, addTask, removeTask, toggleTaskCompletion };
 });
