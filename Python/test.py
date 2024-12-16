54663928f6c35852bc1620bff2e71c168fd3286c
CHEN WEI-CHENG <3331363@gmail.com>
2024-12-16 15:56:04 +0800

    2024.12.16

--- Python/test.py
+++ Python/test.py
@@ -1,9 +1,8 @@
-from socket import socket, SOCK_STREAM, AF_INET
+divided = -15
+temp = -3
+q = -1
+while temp > (divided+temp):  # 這裡修改比較條件
+    temp <<=1
+    q <<=1
+print(temp, q)
 
-client = socket(family=AF_INET, type=SOCK_STREAM)
-# 2. 連接到伺服器(需要指定IP位址和port)
-client.connect(('192.168.137.125', 5500)) # NOTE: 我要連線的server IP(就是寫pi的)
-print('連線成功')
-# 3. 從伺服器接收資料
-print(client.recv(1024).decode('utf-8'))
-client.close()
