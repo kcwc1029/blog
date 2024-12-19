016b0d7f5dd51d650658d9ca12c1bc9ec96cbf3b
CHEN WEI-CHENG <3331363@gmail.com>
2024-12-19 15:10:45 +0800

    2024.12.19

--- Python/Chatbot/Chatbot-TheDevelopmentGuide2024/main.py
+++ Python/Chatbot/Chatbot-TheDevelopmentGuide2024/main.py
@@ -1,47 +1,22 @@
 import os
 from dotenv import load_dotenv
 from openai import OpenAI
-from fastapi import FastAPI, Form, Request
-from typing import Annotated
-from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
-
+"""
+NOTE: 初始化環境變量和 OpenAI API
+使用 dotenv 加載環境變量，並初始化 OpenAI API 密鑰。
+"""
 # 加載 .env 文件
 load_dotenv()
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-openai = OpenAI(api_key=OPENAI_API_KEY)
-chat_history = [{"role": "system", "content": "你是一位教授"}] # 初始化對話歷史
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")  # 從 .env 文件讀取 API 密鑰
+openai = OpenAI(api_key=OPENAI_API_KEY)  # 初始化 OpenAI 客戶端
+ 
+response = openai.images.generate(
+    prompt="幫我生成一個好看的微肉美女",
+    n=1,
+    size="1024x1024"
+)
 
-# 初始化API
-app = FastAPI()
-templates = Jinja2Templates(directory="templates")
-
-
-
-@app.get("/", response_class=HTMLResponse)
-async def chat_page(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "history": chat_history})
-
-
-
-
-
-
-
-
-
-
-
-    # # 添加用戶輸入到對話歷史
-    # chat_history.append({"role": "user", "content": user_input})
-    # # 使用 OpenAI API 生成回應
-    # response = openai.chat.completions.create(
-    #     model="gpt-3.5-turbo",
-    #     messages=chat_history,
-    #     temperature=0.7
-    # )
-    # # 獲取 AI 的回應
-    # ai_response = response.choices[0].message.content
-    # # 添加 AI 回應到對話歷史
-    # chat_history.append({"role": "assistant", "content": ai_response})
-    # return ai_response
+image_url = response.data[0].url
+print(response.data[0])
+print()
+print(image_url)
